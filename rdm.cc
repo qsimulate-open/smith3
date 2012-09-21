f547b7ed236d64bb9b13b947a30d20988d2fe5ad
matthewmacleod <matthewmacleod28@gmail.com>
2012-09-21 16:14:22 -0500

    minor

--- rdm.cc
+++ rdm.cc
@@ -45,7 +45,7 @@ string RDM::generate(string indent, const string tlab, const list<shared_ptr<Ind
   // first let's do the generate_get_block for an rdm..
   tt << indent << "std::vector<size_t> i0hash = vec("; 
   int cntr=0;
-  for (auto iter = index_.rbegin(); iter != index_.rend(); ++iter,++cntr) {
+  for (auto iter = index_.rbegin(); iter != index_.rend(); ++iter, ++cntr) {
     if (iter != index_.rbegin()) tt << ", ";
     tt << (*iter)->str_gen() << "->key()";
   }   
@@ -58,14 +58,14 @@ string RDM::generate(string indent, const string tlab, const list<shared_ptr<Ind
   string iindent = "  ";
   // start sort loops
   int cntl=0;
-  for (auto i = loop.begin(); i != loop.end(); ++i,++cntl) {
+  for (auto i = loop.begin(); i != loop.end(); ++i, ++cntl) {
     // string itag += string((*iter)->num());  // new label this would simplify things for next line
-    tt << indent << "for (int " << itag << (*i)->num() << " =0; " << itag << (*i)->num() << " != " << (*i)->str_gen() <<             "->size(); ++"<< itag << (*i)->num() << ") {" << endl;
+    tt << indent << "for (int " << itag << (*i)->num() << " = 0; " << itag << (*i)->num() << " != " << (*i)->str_gen() <<             "->size(); ++"<< itag << (*i)->num() << ") {" << endl;
     close.push_back(indent + "}");
     indent += iindent;
   }
   // somehow check if we have a delta, if so we need to process and get the indices
-  // and when don't have need to make an exception for data
+  // and when don't have need to make an exception for data addition
 /*
   for (auto d = delta_.begin() ; d != delta_.end(); ++d) {  
     std::cout << (*d)->print() << endl;
@@ -73,10 +73,10 @@ string RDM::generate(string indent, const string tlab, const list<shared_ptr<Ind
 */
 
   // make odata part of summation for target
-  int cnt=0;
-  int cntp=0;
+  int cnt = 0;
+  int cntp = 0;
   ls  << "odata[";
-  for (auto ri = loop.rbegin(); ri != loop.rend(); ++ri,++cnt) {
+  for (auto ri = loop.rbegin(); ri != loop.rend(); ++ri, ++cnt) {
     if (cnt != cntl-1 && cnt != cntl-2) {
       ls << itag << (*ri)->num() << "+" << (*ri)->str_gen() << "->size()*(";
       ++cntp;
@@ -84,17 +84,17 @@ string RDM::generate(string indent, const string tlab, const list<shared_ptr<Ind
       ls << itag << (*ri)->num() << "+" << (*ri)->str_gen() << "->size()*";
     } else {
       ls << itag << (*ri)->num();
-      for (int p=0; p!= cntp; ++p) ls << ")";
+      for (int p = 0; p != cntp; ++p) ls << ")";
       ls << "]" << endl;
     }
   }
   // make data part of summation
   {
-  int cnt=0;
-  int cntp=0;
+  int cnt = 0;
+  int cntp = 0;
   // mkm not sure why this factor isn't looking like a double, ie I get 1 and not 1.0
   rs << "(" << factor() << ") * data[";
-  for (auto riter = index_.rbegin(); riter != index_.rend(); ++riter,++cnt) {
+  for (auto riter = index_.rbegin(); riter != index_.rend(); ++riter, ++cnt) {
     if (cnt != cntr-1 && cnt!= cntr-2) {
       rs << itag << (*riter)->num() << "+" << (*riter)->str_gen() << "->size()*(";
       ++cntp;
@@ -102,7 +102,7 @@ string RDM::generate(string indent, const string tlab, const list<shared_ptr<Ind
       rs << itag << (*riter)->num() << "+" << (*riter)->str_gen() << "->size()*";
     } else {
       rs << itag << (*riter)->num();
-      for (int p=0; p!= cntp; ++p) rs << ")";
+      for (int p = 0; p != cntp; ++p) rs << ")";
       rs << "]";
     }
   }
