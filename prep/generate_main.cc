94110d88f3b0fdc241e96c1d8d475a781519b440
matthewmacleod <matthewmacleod28@gmail.com>
2012-10-21 20:58:00 -0500

    The  option for general CASTP2 excitation is added to the generate_main.cc file. Compiles but debugging is still needed.

--- prep/generate_main.cc
+++ prep/generate_main.cc
@@ -56,11 +56,15 @@ tuple<vector<shared_ptr<Tensor> >, vector<shared_ptr<Tensor> >, vector<shared_pt
     for (auto& j : label) {
       for (auto& k : label) {
         for (auto& l : label) {
-#if 1
+#if 1     // CASPT2 test case
           if ((l == "x" && k == "x" && j == "a" && i == "a")
           ||  (l == "x" && k == "x" && j == "x" && i == "a")) {
-#else
+#endif
+#if 0     // MP2
           if (l == "c" && k == "c" && j == "a" && i == "a") {
+#endif
+#if 0     // general CASPT2 case
+          if (!( l == "x" && k == "x" && j == "x" && i == "x") && !( l == "a" || k == "a" ) && !(j == "c" || i == "c") && !(l == "c" && k == "x") && !(i == "a" && j == "x")) {
 #endif
             stringstream ss; ss << cnt;
             lp.push_back(shared_ptr<Tensor>(new Tensor("proj", ss.str(), {l, k, j, i}))); 
