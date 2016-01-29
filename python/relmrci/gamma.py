8bed842faa24d9d1db86658e0c69f8b3215ad589
Toru Shiozaki <shiozaki@northwestern.edu>
2016-01-29 11:03:30 -0600

    minor fix

--- python/relmrci/gamma.py
+++ python/relmrci/gamma.py
@@ -9,10 +9,10 @@ f.close()
 os.remove("RelMRCI_gamma.cc")
 
 f = open('RelMRCI_gamma.cc', 'w')
-f.write("\n".join(lines[:26]))
+f.write("\n".join(lines[:25]))
 add1 = "\n#include <bagel_config.h>\n#ifdef COMPILE_SMITH\n"
 f.write(add1)
-f.write("\n".join(lines[26:]))
+f.write("\n".join(lines[25:]))
 add2 = "#endif\n"
 f.write(add2)
 
